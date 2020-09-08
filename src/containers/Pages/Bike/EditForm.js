15d678e8064cc0b3c9e125cbbe8cd4ac198c1fa6
gahfaustino <gahfaustino@gmail.com>
2020-09-08 00:08:59 -0300

    add skeleton on bikes edit form

--- src/containers/Pages/Bike/EditForm.js
+++ src/containers/Pages/Bike/EditForm.js
@@ -12,6 +12,8 @@ import { BillingFormWrapper, InputBoxWrapper } from './Checkout.styles';
 import { direction } from '@iso/lib/helpers/rtl';
 import {ToastContainer, toast, Zoom} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Skeleton from '@iso/components/uielements/skeleton';
+import {Divider} from 'antd';
 import api from '../../../helpers';
 
 const { Search } = AntInput;
@@ -255,208 +257,211 @@ export default function() {
     );
 };
 
-  if(data.length === 0) {
-    return <BillingFormWrapper> Nenhum dado encontrado para a  bike.</BillingFormWrapper>;
-  } else {
-    return (
-      <BillingFormWrapper className="isoBillingForm">
-        <Form form={form}
-          layout="vertical"
-          initialValues={{
-            id: data.id,
-            customer_id: data.customer.id,
-            serialNumber: data.serialNumber,
-            biketype: data.biketype,
-            brand: data.brand,
-            model: data.model,
-            color: data.color,
-            forwardExchange: data.forwardExchange,
-            rearDerailleur: data.rearDerailleur,
-            brakeType: data.brakeType,
-            typeSuspension: data.typeSuspension,
-            wheelType: data.wheelType,
-            forkType: data.forkType,
-            frametype: data.frametype,
+if(data.length === 0) {
+  return <>
+    <Skeleton active size="large"/>
+    <Divider />
+    <Skeleton.Button active size="large"/>  
+    </>
+}
+  return (
+    <BillingFormWrapper className="isoBillingForm">
+      <Form form={form}
+        layout="vertical"
+        initialValues={{
+          id: data.id,
+          customer_id: data.customer.id,
+          serialNumber: data.serialNumber,
+          biketype: data.biketype,
+          brand: data.brand,
+          model: data.model,
+          color: data.color,
+          forwardExchange: data.forwardExchange,
+          rearDerailleur: data.rearDerailleur,
+          brakeType: data.brakeType,
+          typeSuspension: data.typeSuspension,
+          wheelType: data.wheelType,
+          forkType: data.forkType,
+          frametype: data.frametype,
 
-          }}
-          onFinish={onFinish}
+        }}
+        onFinish={onFinish}
+      >
+        <Form.Item
+          name="id"
+          label="Código da Bike"
+          rules={[
+            {
+              required: true,
+              message: 'Insira o código da bike!',
+            },
+          ]}
         >
-          <Form.Item
-            name="id"
-            label="Código da Bike"
-            rules={[
-              {
-                required: true,
-                message: 'Insira o código da bike!',
-              },
-            ]}
-          >
-            <Input disabled />
-          </Form.Item>
-          <Form.Item
-            name="customer_id"
-            label="Proprietário"        
-            rules={[
-              {
-                required: true,
-                message: 'Insira o proprietário da bike!',
-              },
-            ]}
-          >
-            <AutoCompletes 
-              url="/search/customer"
-              placeholder="Proprietário"
-            />
-          </Form.Item>
-          <Form.Item
-            name="serialNumber"
-            label="Número de Série"
-            rules={[
-              {
-                required: true,
-                message: 'Insira o número de série da bike!',
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item
-            name="biketype"
-            label="Tipo da Bike"
-            rules={[
-              {
-                required: true,
-                message: 'Selecione o tipo da bike',
-              },
-            ]}
-          >
-            <Select options={types} />
+          <Input disabled />
+        </Form.Item>
+        <Form.Item
+          name="customer_id"
+          label="Proprietário"        
+          rules={[
+            {
+              required: true,
+              message: 'Insira o proprietário da bike!',
+            },
+          ]}
+        >
+          <AutoCompletes 
+            url="/search/customer"
+            placeholder="Proprietário"
+          />
+        </Form.Item>
+        <Form.Item
+          name="serialNumber"
+          label="Número de Série"
+          rules={[
+            {
+              required: true,
+              message: 'Insira o número de série da bike!',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          name="biketype"
+          label="Tipo da Bike"
+          rules={[
+            {
+              required: true,
+              message: 'Selecione o tipo da bike',
+            },
+          ]}
+        >
+          <Select options={types} />
 
-          </Form.Item>
-          <Form.Item
-            name="brand"
-            label="Marca"
-            rules={[
-              {
-                required: true,
-                message: 'Insira a marca da bike!',
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item
-            name="model"
-            label="Modelo"
-            rules={[
-              {
-                required: true,
-                message: 'Insira o modelo da bike!',
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item
-            name="color"
-            label="Cor"
-            rules={[
-              {
-                required: true,
-                message: 'Insira a cor da bike!',
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item
-            name="forwardExchange"
-            label="Cambio Dianteiro"
-            rules={[
-              {
-                required: true,
-                message: 'Insira o tipo de cambio dianteiro da bike!',
-              },
-            ]}
-          >
-            <Input defaultValue={1} suffix="Velocidades"/>
-          </Form.Item>
-          <Form.Item
-            name="rearDerailleur"
-            label="Cambio Traseiro"
-            rules={[
-              {
-                required: true,
-                message: 'Insira o tipo de cambio traseiro da bike!',
-              },
-            ]}
-          >
-            <Input defaultValue={18} suffix="Velocidades"/>
-          </Form.Item>
-          <Form.Item
-            name="brakeType"
-            label="Tipo de Freio"
-            rules={[
-              {
-                required: true,
-                message: 'Selecione o tipo de freio da bike',
-              },
-            ]}
-          >
-            <Select options={brakeTypes} />
-          </Form.Item>
-          <Form.Item
-            name="typeSuspension"
-            label="Tipo de Suspensão"
-            rules={[
-              {
-                required: true,
-                message: 'Selecione o tipo de suspensão da bike',
-              },
-            ]}
-          >
-            <Select options={suspensionTypes} />
-          </Form.Item>
-          <Form.Item
-            name="wheelType"
-            label="Tipo de Aro"
-            rules={[
-              {
-                required: true,
-                message: 'Selecione o tipo de aro da bike',
-              },
-            ]}
-          >
-            <Select options={wheelTypes} />
-          </Form.Item>
-          <Form.Item
-            name="forkType"
-            label="Tipo de Garfo"
-            rules={[
-              {
-                required: true,
-                message: 'Selecione o tipo de garfo da bike',
-              },
-            ]}
-          >
-            <Select options={forkTypes} />
-          </Form.Item>
-          <Form.Item
-            name="frametype"
-            label="Tipo de Quadro"
-            rules={[
-              {
-                required: true,
-                message: 'Selecione o tipo de quadro da bike',
-              },
-            ]}
-          >
-            <Select options={frameTypes} />
-          </Form.Item>
+        </Form.Item>
+        <Form.Item
+          name="brand"
+          label="Marca"
+          rules={[
+            {
+              required: true,
+              message: 'Insira a marca da bike!',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          name="model"
+          label="Modelo"
+          rules={[
+            {
+              required: true,
+              message: 'Insira o modelo da bike!',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          name="color"
+          label="Cor"
+          rules={[
+            {
+              required: true,
+              message: 'Insira a cor da bike!',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          name="forwardExchange"
+          label="Cambio Dianteiro"
+          rules={[
+            {
+              required: true,
+              message: 'Insira o tipo de cambio dianteiro da bike!',
+            },
+          ]}
+        >
+          <Input defaultValue={1} suffix="Velocidades"/>
+        </Form.Item>
+        <Form.Item
+          name="rearDerailleur"
+          label="Cambio Traseiro"
+          rules={[
+            {
+              required: true,
+              message: 'Insira o tipo de cambio traseiro da bike!',
+            },
+          ]}
+        >
+          <Input defaultValue={18} suffix="Velocidades"/>
+        </Form.Item>
+        <Form.Item
+          name="brakeType"
+          label="Tipo de Freio"
+          rules={[
+            {
+              required: true,
+              message: 'Selecione o tipo de freio da bike',
+            },
+          ]}
+        >
+          <Select options={brakeTypes} />
+        </Form.Item>
+        <Form.Item
+          name="typeSuspension"
+          label="Tipo de Suspensão"
+          rules={[
+            {
+              required: true,
+              message: 'Selecione o tipo de suspensão da bike',
+            },
+          ]}
+        >
+          <Select options={suspensionTypes} />
+        </Form.Item>
+        <Form.Item
+          name="wheelType"
+          label="Tipo de Aro"
+          rules={[
+            {
+              required: true,
+              message: 'Selecione o tipo de aro da bike',
+            },
+          ]}
+        >
+          <Select options={wheelTypes} />
+        </Form.Item>
+        <Form.Item
+          name="forkType"
+          label="Tipo de Garfo"
+          rules={[
+            {
+              required: true,
+              message: 'Selecione o tipo de garfo da bike',
+            },
+          ]}
+        >
+          <Select options={forkTypes} />
+        </Form.Item>
+        <Form.Item
+          name="frametype"
+          label="Tipo de Quadro"
+          rules={[
+            {
+              required: true,
+              message: 'Selecione o tipo de quadro da bike',
+            },
+          ]}
+        >
+          <Select options={frameTypes} />
+        </Form.Item>
 
-          <Button disabled={disabled} htmlType="submit">Salvar</Button>
-        </Form> 
-      </BillingFormWrapper>
-    );
-  }
+        <Button disabled={disabled} htmlType="submit">Salvar</Button>
+      </Form> 
+    </BillingFormWrapper>
+  );
 }
